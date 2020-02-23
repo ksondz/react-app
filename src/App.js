50901746643e6d64ba7884e706323df3231aa3f7
kostiantyn <kostiantyn.ksondz@gmail.com>
2020-02-23 20:05:33 +0200

    feature: first example

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          It was changed over the feature branch
         </a>
       </header>
     </div>
